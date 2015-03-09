dff546df8f34a4d37584414abbad2a2bcdd7f613
Rachel Lee Campbell <rachelleecampbell@gmail.com>
2015-03-09 13:58:08 -0500

    reinitialized files

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,24 @@
+class HomeController < ApplicationController
+  def index
+  
+    right_now = Time.now
+    @current_time = right_now.stamp_like("2:00 PM")
+    
+# remember to finish the string with .html_safe
+      @greet_message = case right_now.hour
+      when 5..11
+        "Good Morning! <i class='fa fa-bicycle'></i>".html_safe
+      when 12..15
+        "Good Afternoon! <i class='fa fa-beer'></i>".html_safe
+      when 16..19
+        "Good Evening! <i class='fa fa-beer'></i>".html_safe
+      when 20..23, 0..4
+        "Good Night! Early to bed early to shred <i class='fa fa-bed'></i>".html_safe
+      end 
+
+
+    @quote = Quote.all.sample.quip
+
+ end
+end
+
---
+++ db/seeds.rb
@@ -0,0 +1,45 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Quote.create(quip: "What makes a great endurance athlete is the ability to absorb potenial embarrassment, and to suffer without complaint. I was discovering that if it was a matter of gritting my teeth, not caring how it looked, and outlasting everybody else, I won. It didn't seem to matter what sport it was--in a straight-ahead, long-distant race, I could beat anybody. If it was a suffer-fest, I was good at it. -Lance Armstrong")
+Quote.create(quip: "But to say that the race is the metaphor for the life is to miss the point. The race is everything. It obliterates whatever isn't racing. Life is the metaphor for the race. -Donald Antrim")
+Quote.create(quip: "I thought of that while riding my bike. -Albert Einstein | On the Theory of Relativity.")
+Quote.create(quip: "When the spirits are low, when the day appears dark, when work becomes monotonous, when hope hardly seems worth having, just mount a bicycle and go out for a spin down the road, without thought on anything but the ride you are taking. -Arthur Conan Doyle")
+Quote.create(quip: "A bicycle ride is a flight from sadness. -James E. Starrs | The Literary Cyclist.")
+Quote.create(quip: "For all their fearlessness, sprinters are a delicate breed. At the peak of form they effuse an aura of invincibility, suggesting no bicycle rider ever pedaled as fast. To win, sprinters must have everything: physical condition, confidence, luck, aggression and committed team support. An elusive combination, attainable but not sustainable. -David Walsh | Inside the Tour de France.")
+Quote.create(quip: "You have to sprint on feeling, not thinking. You must have faith in yourself but you cannot think about it too much. -Jean Paul Van Poppel")
+Quote.create(quip: "Let me tell you what I think of bicycling. I think it has done more to emancipate women than anything else in the world. It gives women a feeling of freedom and self—reliance. I stand and rejoice every time I see a woman ride by on a wheel...the picture of free, untrammeled womanhood. -Susan B. Anthony")
+Quote.create(quip: "When I was a kid I used to pray every night for a new bicycle. Then I realised that the Lord doesn't work that way so I stole one and asked Him to forgive me. -Emo Philips")
+Quote.create(quip: "Perhaps the single most important element in mastering the techniques and tactics of racing is experience. But once you have the fundamentals, acquiring the experience is a matter of time. -Greg LeMond")
+Quote.create(quip: "Pain is a big fat creature riding on your back. The farther you pedal, the heavier he feels. The harder you push, the tighter he squeezes your chest. The steeper the climb, the deeper he digs his jagged, sharp claws into your muscles. -Scott Martin")
+Quote.create(quip: "The bicycle is just as good company as most husbands and, when it gets old and shabby, a woman can dispose of it and get a new one without shocking the entire community. -Ann Strong | Minneapolis Tribune, 1895.")
+Quote.create(quip: "There was a second supremely sweet moment of victory. As I made my way through the finish area, I passed the Cofidis team. Assorted members of the organization stood around, the men who I felt had left me for dead in a hospital room. 'That was for you,' I said as I moved past them. -Lance Armstrong | After winning the opening time trial and becoming the leader of 1999 Tour de France.")
+Quote.create(quip: "If you worried about falling off the bike, you'd never get on. -Lance Armstrong")
+Quote.create(quip: "I have always struggled to achieve excellence. One thing that cycling has taught me is that if you can achieve something without a struggle it's not going to be satisfying. -Greg LeMond")
+Quote.create(quip: "She who succeeds in gaining the mastery of the bicycle will gain the mastery of life. -Frances E. Willard | How I Learned to Ride the Bicycle.")
+Quote.create(quip: "The hardest part of raising a child is teaching them to ride bicycles. A shaky child on a bicycle for the first time needs both support and freedom. The realization that this is what the child will always need can hit hard. -Sloan Wilson")
+Quote.create(quip: "When I see an adult on a bicycle, I do not despair for the future of the human race. - H.G. Wells")
+Quote.create(quip: "Wheel, kindly light, along life's cycle path, Wheel Thou on me! The road is rough, I have discerned Thy Wrath, But wheel me on! -Christian Hymn")
+Quote.create(quip: "For instance, the bicycle is the most efficient machine ever created: Converting calories into gas, a bicycle gets the equivalent of three thousand miles per gallon. -Bill Strickland | The Quotable Cyclist.")
+Quote.create(quip: "Until mountain biking came along, the bike scene was ruled by a small elite cadre of people who seemed allergic to enthusiasm. -Jacquie Phelan")
+Quote.create(quip: "Get a bicycle. You will not regret it if you live. - Mark Twain | Taming the Bicycle.")
+Quote.create(quip: "Life is like riding a bicycle - in order to keep your balance, you must keep moving. -Albert Einstein")
+Quote.create(quip: "Nothing compares to the simple pleasure of a bike ride. -John F. Kennedy")
+Quote.create(quip: "Bicycling is the nearest approximation I know to the flight of birds. The airplane simply carries a man on its back like an obedient Pegasus; it gives him no wings of his own. -Louis J. Helle, Jr. | Spring in Washington.")
+Quote.create(quip: "The secret to mountain biking is pretty simple. The slower you go the more likely it is you'll crash. -Julie Furtado")
+Quote.create(quip: "Things look different from the seat of a bike carrying a sleeping bag with a cold beer tucked inside. -Jim Malusa")
+Quote.create(quip: "All creatures who have ever walked have wished that they might fly. With highwheelers a flesh and blood man can hitch wings to his feet. -Karl Kron | Ten Thousand Miles on a Bicycle.")
+Quote.create(quip:"Caution is the key to safe cycling. I'm aware that cars are bigger than me, but I feel quite safe. I'm in control, liberated and free, when I'm on my bike. -Erin O'Connor")
+Quote.create(quip: "If constellations had been named in the 20th century, I suppose we would see bicycles. -Carl Sagan")
+Quote.create(quip: "Cyclers see considerable more of this beautiful world than any other class of citizens. A good bicycle, well applied, will cure most ills this flesh is heir to. - Dr. K. K. Doty")
+
+
+
+
+
+
